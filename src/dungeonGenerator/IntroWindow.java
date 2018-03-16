d54732ce67be26d0a0f8c9c99a8a42ec842d3d49
Tanner Braun <tsbraun1891@gmail.com>
2018-03-16 10:53:10 -0400

    Added next button. Textfields now collect information. Changed background to lighten the mood.

--- src/dungeonGenerator/IntroWindow.java
+++ src/dungeonGenerator/IntroWindow.java
@@ -12,6 +12,7 @@ public class IntroWindow extends JPanel{  //First window of the application that
     private JLabel partySizeL, mapSizeL;
     private JTextField partySize, mapSize;
     private int roomNumber;
+    private JButton nextButton;
 
     IntroWindow() {
         panel = new JPanel(); //Set Layout to Box?
@@ -23,17 +24,37 @@ public class IntroWindow extends JPanel{  //First window of the application that
         mapSizeL = new JLabel("Dungeon Size: ");  // Radio buttons to receive the desired size of the dungeon
         mapSize = new JTextField(5);
 
+        nextButton = new JButton("Next");
+        nextButton.addActionListener(new NextListener());
 
         partySizeL.setVisible(true);
-        panel.setBackground(Color.WHITE);
+        panel.setBackground(Color.MAGENTA);
         panel.add(partySizeL);
         panel.add(partySize);
         panel.add(mapSizeL);
         panel.add(mapSize);
+        panel.add(nextButton);
         panel.setVisible(true);
     }
 
     public JPanel get_IntroPanel() {
         return panel;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    private class NextListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String size = mapSize.getText();
+            if (size.equals("")) {
+                roomNumber = 0; // Display error message or throw exception
+            } else {
+                roomNumber = Integer.parseInt(size);
+            }
+            mapSizeL.setText("Dungeon Size: " + roomNumber);
+        }
     }
 }
